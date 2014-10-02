0e014e40e77c67d079b6978e9c76a11d30efb9db
Jon Deal <jon.deal@gmail.com>
2014-10-02 11:41:23 -0600

    list not neccesary

--- lib/bronto/field.rb
+++ lib/bronto/field.rb
@@ -1,9 +1,9 @@
 module Bronto
   class Field < Base
-    attr_accessor :id, :name, :label, :type, :visibility, :listIds
+    attr_accessor :id, :name, :label, :type, :visibility
 
     def to_hash
-      hash = { name: name, label: label, type: type, visibility: visibility, listIds: listIds }
+      hash = { name: name, label: label, type: type, visibility: visibility }
       hash[:id] = id if id.present?
       hash
     end
