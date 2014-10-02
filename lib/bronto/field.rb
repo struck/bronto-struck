2dfcd90a17d6c9e0490b0c5f4a3e1ef466569360
Jon Deal <jon.deal@gmail.com>
2014-10-02 10:21:46 -0600

    Wait, maybe capitalization matters

--- lib/bronto/field.rb
+++ lib/bronto/field.rb
@@ -1,9 +1,9 @@
 module Bronto
   class Field < Base
-    attr_accessor :id, :name, :label, :type, :visibility
+    attr_accessor :id, :name, :label, :type, :visibility, :listIds
 
     def to_hash
-      hash = { name: name, label: label, type: type, visibility: visibility }
+      hash = { name: name, label: label, type: type, visibility: visibility, :listIds listIds }
       hash[:id] = id if id.present?
       hash
     end
