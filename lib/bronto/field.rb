586da65862111c0c91a542e54cec15daf242a5e8
Jon Deal <jon.deal@gmail.com>
2014-10-02 10:17:22 -0600

    Typo?

--- lib/bronto/field.rb
+++ lib/bronto/field.rb
@@ -3,7 +3,7 @@ module Bronto
     attr_accessor :id, :name, :label, :type, :visibility, :listIds
 
     def to_hash
-      hash = { name: name, label: label, type: type, visibility: visibility, :listIds listid }
+      hash = { name: name, label: label, type: type, visibility: visibility, :listIds listids }
       hash[:id] = id if id.present?
       hash
     end
